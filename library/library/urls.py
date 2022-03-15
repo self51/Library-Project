b3662ccefa06dda4e9b0215015f0cf6b1fd8ecec
se1f4 <58149320+se1f4@users.noreply.github.com>
2022-03-15 22:02:57 +0200

    Added home page, update base.html

--- library/library/urls.py
+++ library/library/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('authentication.urls')),
     path('author/', include('author.urls')),
