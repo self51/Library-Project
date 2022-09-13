b84d59cecbd14f2340194b817e2f4e90d9b539a0
se1f4 <58149320+se1f4@users.noreply.github.com>
2022-09-13 17:21:28 +0300

    Fixed book counting bug and added alert

--- library/order/views.py
+++ library/order/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.contrib.auth.models import User
+from django.contrib import messages
+
 from .models import Order
 from book.models import Book
 from .forms import OrderForm
@@ -16,19 +18,35 @@ def user_orders(request, pk):
     orders = Order.objects.filter(user=user)
     return render(request, 'order/user_orders.html', {'orders': orders})
 
+
 def order_form(request, pk=None):
     if request.user.is_authenticated:
         book = Book.objects.get(id=pk)
         form = OrderForm(request.POST)
-        if form.is_valid():
+
+        if form.is_valid() and book.count > 0:
             order = form.save(commit=False)
             order.user = request.user
             order.book = book
             form.save()
-        return redirect('/order/')
+
+            book.count -= 1
+            book.save()
+
+        else:
+            messages.warning(request, 'Not available now')
+            return redirect('/book/')
+
+        return redirect('user_orders', pk=request.user.id)
     return redirect('login')
+
 
 def order_delete(request, pk):
     order = Order.objects.get(id=pk)
     order.delete()
-    return redirect('account')
+    book = order.book
+    book.count += 1
+    book.save()
+
+    messages.success(request, 'The book was successfully returned!')
+    return redirect('user_orders', pk=request.user.id)
