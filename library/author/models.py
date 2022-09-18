fef7ba0336de96f564aa8254c05da4312e36f728
se1f4 <58149320+se1f4@users.noreply.github.com>
2022-09-18 14:41:03 +0300

    Fixed models bug

--- library/author/models.py
+++ library/author/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Author(models.Model):
-    name = models.CharField(blank=True, max_length=20)
-    surname = models.CharField(blank=True, max_length=20)
-    patronymic = models.CharField(blank=True, max_length=20)
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+    patronymic = models.CharField(max_length=20)
 
     def __str__(self):
         return str(self.name + ' ' + self.surname + ' ' + self.patronymic)
