a74b708731bd69558bead6cec4018882c7701336
se1f4 <58149320+se1f4@users.noreply.github.com>
2022-09-18 18:04:33 +0300

    wrote unit tests for views

--- library/author/tests.py
+++ library/author/tests.py
@@ -1,6 +1,7 @@
-from django.test import TestCase, SimpleTestCase
+from django.test import TestCase, SimpleTestCase, Client
 from django.urls import reverse, resolve
 
+from author.models import Author
 from author.views import AuthorListView, author_form, author_delete, author_books
 
 
@@ -25,4 +26,66 @@ class TestUrl(SimpleTestCase):
     def test_author_books_url_resolves(self):
         url = reverse('author_books', args=[1])
         self.assertEqual(resolve(url).func, author_books)
+
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.author_list = reverse('author_list')
+        self.author_books = reverse('author_books', args=[1])
+        self.author_add = reverse('author_form')
+        self.author = Author.objects.create(
+            name="Person",
+            surname="Person_surname",
+            patronymic="Person_patronymic",
+        )
+
+    def test_author_list_view(self):
+        response = self.client.get(self.author_list)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'author/author_list.html')
+
+    def test_author_books(self):
+        response = self.client.get(self.author_books)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'book/book_list.html')
+
+    def test_author_form_get(self):
+        response_add = self.client.get(self.author_add)
+        response_update = self.client.get(reverse('author_update', args=[3]))
+
+        self.assertEqual(response_add.status_code, 200)
+        self.assertTemplateUsed(response_add, 'author/author_form.html')
+        self.assertEqual(response_update.status_code, 200)
+        self.assertTemplateUsed(response_update, 'author/author_form.html')
+
+    def test_author_form_post(self):
+        response_add = self.client.post(self.author_add, {
+            'name': 'Person1',
+            'surname': "Person_surname",
+            'patronymic': "Person_patronymic"
+        })
+
+
+        self.assertEqual(response_add.status_code, 302)
+        self.assertEqual(Author.objects.get(id=5).name, 'Person1')
+
+        response_update = self.client.post(reverse('author_update', args=[4]), {
+            'name': 'name1',
+            'surname': "Person_surname",
+            'patronymic': "Person_patronymic"
+        })
+
+        self.assertEqual(response_update.status_code, 302)
+        self.assertEqual(Author.objects.get(id=4).name, 'name1')
+
+
+    def test_author_delete(self):
+        response = self.client.delete(reverse('author_delete', args=[2]))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Author.objects.all().count(), 0)
 
